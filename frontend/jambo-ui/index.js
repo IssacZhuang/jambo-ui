cdbee161e538beb43f300185ee4b8a087f49350e
IssacZhuang <1095313400@qq.com>
2019-06-20 16:42:50 +0800

    更新了toggle

--- frontend/jambo-ui/index.js
+++ frontend/jambo-ui/index.js
@@ -1,2 +1,11 @@
 import './css/index.scss';
 
+var $ = function (arg) {
+    return document.querySelector(arg);
+}
+
+window.onload=function(){
+    $('#menu-button').onclick = function () {
+        $('.nav-link-container').classList.toggle('open');
+    }
+}
