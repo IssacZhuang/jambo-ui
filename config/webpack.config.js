61bfb9b93a80cb66227469b318f230f3cd4ac6d1
unknown <1049825131@qq.com>
2019-06-25 16:58:49 +0800

    弹窗

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -43,6 +43,7 @@ module.exports = {
             {
                 test: /\.js$/,
                 loader: 'babel-loader',
+                query: {presets: ['es2015']},
                 exclude: /node_modules/
             },
             {
