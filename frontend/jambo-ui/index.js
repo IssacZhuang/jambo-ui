a591b8336056da9c2aabd9a1e0975f71568d6654
unknown <1049825131@qq.com>
2019-06-26 15:54:08 +0800

    用户名密码为空提醒

--- frontend/jambo-ui/index.js
+++ frontend/jambo-ui/index.js
@@ -1,9 +1,8 @@
 import './css/index.scss';
-import Axios from 'axios';
 
 window.onload = function () {
     const header = document.body.querySelector('.navbar');
-    console.log(header);
+    const modal =document.body.querySelector('.modal');
 
     header.querySelector('#menu-button').onclick = function () {
         header.querySelector('.nav-link-container').classList.toggle('open');
@@ -16,4 +15,28 @@ window.onload = function () {
         header.querySelector('.modal-container').classList.remove('open');
         document.body.classList.remove('modal-opened');
     }
+    function feedback(object){
+        console.log(object);
+        let feedback = object.parentElement.querySelector('.feedback');
+        if(object.value==''){
+            feedback.innerHTML='用户名不能为空';
+            feedback.classList.add('error');
+            return false;
+        }else{
+            feedback.innerHTML='';
+            feedback.classList.remove('error');
+            return true;
+        }
+    }
+    modal.querySelector('#username').onchange = function() {
+       feedback(this);
+    };
+    modal.querySelector('#password').onchange = function() {
+        feedback(this);
+    };
+    modal.querySelector('.btn').onclick = function(){
+        let isUsernameValid = feedback(modal.querySelector('#username'));
+        let isPasswordValid = feedback(modal.querySelector('#password'));
+    }
 }
+
